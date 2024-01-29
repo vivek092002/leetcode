60427eb89213aebc6476162871e9c699f9d3311d
kumarvivek <kumar.vivek092002@gmail.com>
2024-01-30 02:28:41 +0530

    30 jan 2024

---
+++ a26removeDupliactes.java
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+import java.util.Scanner;
+
+public class a26removeDupliactes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        //input array
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        int ans = removeDuplicates(nums);
+        System.out.println(ans);
+        sc.close();
+    }
+    public static int removeDuplicates(int[] nums) {
+        int i=0;
+        for(int j=1 ; j<nums.length ; j++){
+            if(nums[j] != nums[i]){
+                nums[i+1] = nums[j];
+                i++;
+            }
+        }
+        return (i+1);
+    }
+}
